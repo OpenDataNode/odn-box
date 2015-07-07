ef9a28534bb23069ab8b8484694f0e82cff1b840
Jan Marcek <jan.marcek@eea.sk>
2015-07-07 10:37:55 +0200

    added migration functionality with user_actor id

--- debian/test.sh
+++ debian/test.sh
@@ -16,6 +16,12 @@ getNewUserName() {
     echo "$username"
 }
 
+getUserActorId() {
+    userId=$1
+    username=`su - postgres -c  "psql -A -t -d unifiedviews -c \"select user_actor.id from usr_extuser INNER JOIN user_actor ON usr_extuser.id_extuser = user_actor.id_extuser  where id_usr= ${userId};\""`
+    echo "$username"
+}
+
 su - postgres -c  "psql -A -t -d unifiedviews -c \"delete from  usr_organization;\""
 
 result=`ldapsearch -x -D cn=idm,ou=Administrators,dc=opendata,dc=org  -b  ou=people,dc=opendata,dc=org  -w secret  objectClass=inetOrgPerson |  grep "uid:\|ou:"`
@@ -35,9 +41,71 @@ su - postgres -c  "psql -A -t -d unifiedviews -c \"drop table IF EXISTS tmp;\""
 su - postgres -c  "psql -A -t -d unifiedviews -c \"create table tmp as table usr_user;\" "
 su - postgres -c  "psql -A -t -d unifiedviews -c \"select concat('INSERT INTO  \"usr_user\" VALUES (nextval(''seq_usr_user''),''', organization,''' ,1,''100000:3069f2086098a66ec0a859ec7872b09af7866bc7ecafe2bed3ec394454056db2:b5ab4961ae8ad7775b3b568145060fabb76d7bca41c7b535887201f79ee9788a'',', ' ''', organization,'''',',20);') from usr_organization;\"" > update.sql
 su - postgres -c "psql  -d ${dbname}" <  update.sql
+
+su - postgres -c  "psql -A -t -d unifiedviews -c \"select concat('INSERT INTO  \"user_actor\" VALUES (nextval(''seq_usr_user''),''', username,''', ''',full_name,''' );') from usr_user;\"" > user_actors.sql
+su - postgres -c "psql  -d ${dbname}" <  user_actors.sql
+
 su - postgres -c  "psql -A -t -d unifiedviews -c \"ALTER TABLE  ppl_model  DROP CONSTRAINT ppl_model_pkey CASCADE\" "
 su - postgres -c  "psql -A -t -d unifiedviews -c \"ALTER TABLE  ppl_model  DROP CONSTRAINT ppl_model_name_key CASCADE;\" "
 su - postgres -c  "psql -A -t -d unifiedviews -c \"ALTER TABLE  ppl_model  DROP CONSTRAINT ppl_model_user_id_fkey  CASCADE;\" "
+
+
+
+echo "update ppl_model"
+# update user_id ppl_model
+list=`su - postgres -c  "psql -A -t -d unifiedviews -c \" select user_id from ppl_model;\""`
+while read -r user_id; do
+    if [ ! -z $user_id ] ; then
+        newUserId=`getNewUserId  $user_id`
+        userActorId=`getUserActorId $user_id`
+        su - postgres -c  "psql -A -t -d unifiedviews -c \"UPDATE ppl_model set user_id = ${newUserId}, user_actor_id=${userActorId} where user_id = ${user_id};\""
+        echo "old id : " $user_id " new id: "$newUserId
+    fi
+  
+done <<< "$list"
+
+
+echo "update exec_pipeline"
+list=`su - postgres -c  "psql -A -t -d unifiedviews -c \" select owner_id from exec_pipeline;\""`
+while read -r user_id; do
+    if [ ! -z $user_id ] ; then
+        newUserId=`getNewUserId  $user_id`
+        if [ ! -z  $newUserId ] ; then
+            userActorId=`getUserActorId $user_id`
+            su - postgres -c  "psql -A -t -d unifiedviews -c \"UPDATE exec_pipeline set owner_id = ${newUserId}, user_actor_id=${userActorId} where owner_id = ${user_id};\""
+            echo "old id : " $user_id " new id: "$newUserId
+        fi
+    fi
+done <<< "$list"
+
+
+echo "update exec_schedule"
+list=`su - postgres -c  "psql -A -t -d unifiedviews -c \" select user_id from exec_schedule;\""`
+while read -r user_id; do
+    if [ ! -z $user_id ] ; then
+        newUserId=`getNewUserId  $user_id`
+         if [ ! -z  $newUserId ] ; then
+                userActorId=`getUserActorId $user_id`
+                su - postgres -c  "psql -A -t -d unifiedviews -c \"UPDATE exec_schedule set user_id = ${newUserId}, user_actor_id=${userActorId} where user_id = ${user_id};\""
+                echo "old id : " $user_id " new id: "$newUserId
+         fi        
+    fi
+   
+done <<< "$list"
+
+
+echo "update dpu_template"
+list=`su - postgres -c  "psql -A -t -d unifiedviews -c \" select user_id from dpu_template;\""`
+while read -r user_id; do
+    if [ ! -z $user_id ] ; then
+        newUserId=`getNewUserId  $user_id`
+        if [ ! -z  $newUserId ] ; then
+            su - postgres -c  "psql -A -t -d unifiedviews -c \"UPDATE dpu_template set user_id = ${newUserId} where user_id = ${user_id};\""
+            echo "old id : " $user_id " new id: "$newUserId
+        fi        
+    fi
+  
+done <<< "$list"
 
 
 echo "update usr_extuser"
@@ -51,47 +119,6 @@ while read -r user_id; do
     fi
 done <<< "$list"
 
-echo "update ppl_model"
-# update user_id ppl_model
-list=`su - postgres -c  "psql -A -t -d unifiedviews -c \" select user_id from ppl_model;\""`
-while read -r user_id; do
-    newUserId=`getNewUserId  $user_id`
-    su - postgres -c  "psql -A -t -d unifiedviews -c \"UPDATE ppl_model set user_id = ${newUserId} where user_id = ${user_id};\""
-    echo "old id : " $user_id " new id: "$newUserId
-done <<< "$list"
-
-
-echo "update exec_pipeline"
-list=`su - postgres -c  "psql -A -t -d unifiedviews -c \" select owner_id from exec_pipeline;\""`
-while read -r user_id; do
-    newUserId=`getNewUserId  $user_id`
-    if [ ! -z  $newUserId ] ; then
-        su - postgres -c  "psql -A -t -d unifiedviews -c \"UPDATE exec_pipeline  set owner_id = ${newUserId} where owner_id = ${user_id};\""
-        echo "old id : " $user_id " new id: "$newUserId
-    fi
-done <<< "$list"
-
-
-echo "update exec_schedule"
-list=`su - postgres -c  "psql -A -t -d unifiedviews -c \" select user_id from exec_schedule;\""`
-while read -r user_id; do
-    newUserId=`getNewUserId  $user_id`
-    if [ ! -z  $newUserId ] ; then
-        su - postgres -c  "psql -A -t -d unifiedviews -c \"UPDATE exec_schedule set user_id = ${newUserId} where user_id = ${user_id};\""
-        echo "old id : " $user_id " new id: "$newUserId
-    fi
-done <<< "$list"
-
-
-echo "update dpu_template"
-list=`su - postgres -c  "psql -A -t -d unifiedviews -c \" select user_id from dpu_template;\""`
-while read -r user_id; do
-    newUserId=`getNewUserId  $user_id`
-    if [ ! -z  $newUserId ] ; then
-        su - postgres -c  "psql -A -t -d unifiedviews -c \"UPDATE dpu_template set user_id = ${newUserId} where user_id = ${user_id};\""
-        echo "old id : " $user_id " new id: "$newUserId
-    fi
-done <<< "$list"
 
 echo "delete old accounts"
 su - postgres -c  "psql -A -t -d unifiedviews -c \"delete from usr_user where id <= ${HIGHEST_USER_ID}; \""
